559af3866ae1cc397cb79aabaa02df41cd83e44f
Rhythm-ONE <rhythmvalidator@gmail.com>
2021-10-11 13:19:30 -0400

    Added confirmation dialog for withdrawals

--- commands/onewithdraw.js
+++ commands/onewithdraw.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { getWalletPrivateKey } = require('../tools/user-wallet');
 const { getBalance, sendTransaction, getAddress } = require('../tools/harmony-util');
+const { MessageActionRow, MessageSelectMenu, MessageButton } = require('discord.js')
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -35,9 +36,49 @@ module.exports = {
 			return interaction.editReply(`Insufficient balance. Current balance: \`${oneBalance}\` $ONE`)
 		}
 
-		const transaction = await sendTransaction(senderPrivateKey, receiverAddress, amount);
+		const row = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setCustomId('confirm')
+					.setLabel('Submit')
+					.setStyle('PRIMARY'),
+				new MessageButton()
+					.setCustomId('cancel')
+					.setLabel('Cancel')
+					.setStyle('SECONDARY'),
+			);
 
-		return interaction.editReply(
-            `Your withdrawal of \`${amount}\` $ONE was successful. \nTransaction details can be found [HERE](<https://explorer.testnet.harmony.one/tx/${transaction.result}>)`);
+		interaction.editReply({
+			content: `Are you sure you'd like to withdraw \`${amount}\` $ONE to address \`${receiverAddress}\`?`,
+			components: [row]
+		});
+
+		const filter = i => {
+			i.deferUpdate();
+			return i.user.id === interaction.user.id;
+		};
+
+        const reply = await interaction.fetchReply();
+
+		var selection = await reply.awaitMessageComponent({ filter, componentType: 'BUTTON', time: 30000 })
+			.then(buttonInteraction => {
+                return buttonInteraction.customId;
+            }).catch(err => {
+                return 'timeout';
+			});
+
+        switch(selection) {
+            case 'confirm':
+                interaction.editReply({content: 'Working on it...', components: []});
+                const transaction = await sendTransaction(senderPrivateKey, receiverAddress, amount);
+                return interaction.editReply(
+                    `Your withdrawal of \`${amount}\` $ONE to address \`${receiverAddress}\` was successful.\nTransaction details can be found [HERE](<https://explorer.testnet.harmony.one/tx/${transaction.result}>)`);
+            case 'cancel':
+                return interaction.editReply({content: 'Withdrawal canceled', components: []});
+            case 'timeout':
+                return interaction.editReply({content: 'Withdrawal automatically canceled due to user interaction timeout', components: []});
+            default:
+                return interaction.editReply('Unknown error');
+        }
 	},
 };
