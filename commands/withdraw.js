56d540ae21c36da13e131e68279b9d4bc1644d14
Rhythm-ONE <rhythmvalidator@gmail.com>
2022-04-23 23:58:20 -0400

    comment out broken confirmation for withdrawal

--- commands/withdraw.js
+++ commands/withdraw.js
@@ -37,55 +37,64 @@ module.exports = {
             return interaction.editReply(`Insufficient balance. Current balance: \`${senderBalance}\` $ONE`)
         }
 
-        const row = new MessageActionRow()
-            .addComponents(
-                new MessageButton()
-                    .setCustomId('confirm')
-                    .setLabel('Submit')
-                    .setStyle('PRIMARY'),
-                new MessageButton()
-                    .setCustomId('cancel')
-                    .setLabel('Cancel')
-                    .setStyle('SECONDARY'),
-            );
-
-        await interaction.editReply({
-            content: `Are you sure you'd like to withdraw \`${amount}\` $ONE to address \`${receiverAddress}\`?`,
-            components: [row]
-        });
-
-        const filter = i => {
-            i.deferUpdate();
-            return i.user.id === interaction.user.id;
-        };
-
-        const reply = await interaction.fetchReply();
-
-        var selection = await reply.awaitMessageComponent({ filter, componentType: 'BUTTON', time: 30000 })
-            .then(buttonInteraction => {
-                return buttonInteraction.customId;
-            }).catch(err => {
-                return 'timeout';
-            });
-
-        switch (selection) {
-            case 'confirm':
-                interaction.editReply({ content: 'Working on it...', components: [] });
-                const transactionResult = await sendTransaction(senderPrivateKey, receiverAddress, amount);
-                if (!!transactionResult.error) {
-                    return interaction.editReply(`Error sending transaction: ${transactionResult.error.message}`);
-                }
-                if (!!transactionResult.result) {
-                    return interaction.editReply(
-                        `Your withdrawal of \`${amount}\` $ONE to address \`${receiverAddress}\` was successful.\nTransaction details can be found [HERE](<${explorerBaseUrl}${transactionResult.result}>)`);
-                }
-                return interaction.editReply({ content: 'Unknown error', components: [] });
-            case 'cancel':
-                return interaction.editReply({ content: 'Withdrawal canceled', components: [] });
-            case 'timeout':
-                return interaction.editReply({ content: 'Withdrawal automatically canceled due to user interaction timeout', components: [] });
-            default:
-                return interaction.editReply({ content: 'Unknown error', components: [] });
+        const transactionResult = await sendTransaction(senderPrivateKey, receiverAddress, amount);
+        if (!!transactionResult.error) {
+            return interaction.editReply(`Error sending transaction: ${transactionResult.error.message}`);
         }
+        if (!!transactionResult.result) {
+            return interaction.editReply(
+                `Your withdrawal of \`${amount}\` $ONE to address \`${receiverAddress}\` was successful.\nTransaction details can be found [HERE](<${explorerBaseUrl}${transactionResult.result}>)`);
+        }
+
+        // const row = new MessageActionRow()
+        //     .addComponents(
+        //         new MessageButton()
+        //             .setCustomId('confirm')
+        //             .setLabel('Submit')
+        //             .setStyle('PRIMARY'),
+        //         new MessageButton()
+        //             .setCustomId('cancel')
+        //             .setLabel('Cancel')
+        //             .setStyle('SECONDARY'),
+        //     );
+
+        // const reply = await interaction.editReply({
+        //     content: `Are you sure you'd like to withdraw \`${amount}\` $ONE to address \`${receiverAddress}\`?`,
+        //     components: [row]
+        // });
+
+        // const filter = i => {
+        //     i.deferUpdate();
+        //     return i.user.id === interaction.user.id;
+        // };
+
+        // console.log(reply);
+
+        // var selection = await reply.awaitMessageComponent({ filter, componentType: 'BUTTON', time: 30000 })
+        //     .then(buttonInteraction => {
+        //         return buttonInteraction.customId;
+        //     }).catch(err => {
+        //         return 'timeout';
+        //     });
+
+        // switch (selection) {
+        //     case 'confirm':
+        //         interaction.editReply({ content: 'Working on it...', components: [] });
+        //         const transactionResult = await sendTransaction(senderPrivateKey, receiverAddress, amount);
+        //         if (!!transactionResult.error) {
+        //             return interaction.editReply(`Error sending transaction: ${transactionResult.error.message}`);
+        //         }
+        //         if (!!transactionResult.result) {
+        //             return interaction.editReply(
+        //                 `Your withdrawal of \`${amount}\` $ONE to address \`${receiverAddress}\` was successful.\nTransaction details can be found [HERE](<${explorerBaseUrl}${transactionResult.result}>)`);
+        //         }
+        //         return interaction.editReply({ content: 'Unknown error', components: [] });
+        //     case 'cancel':
+        //         return interaction.editReply({ content: 'Withdrawal canceled', components: [] });
+        //     case 'timeout':
+        //         return interaction.editReply({ content: 'Withdrawal automatically canceled due to user interaction timeout', components: [] });
+        //     default:
+        //         return interaction.editReply({ content: 'Unknown error', components: [] });
+        // }
     },
 };
